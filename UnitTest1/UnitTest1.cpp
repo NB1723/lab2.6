0776456eccfd37f976a67b326e7de0bc7287deb4
NB1723 <notalia03@gmail.com>
2021-05-18 09:17:39 +0300

    lab2.6

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lorry.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Lorry c;
+			c.setPower(23000);
+			int power = c.getPower();
+			Assert::AreEqual(power, 23000);
+		}
+	};
+}
